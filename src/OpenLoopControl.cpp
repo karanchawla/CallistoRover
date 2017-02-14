5305642cfe24f3ec9a21000d8756a8c0d359884c
Karan Chawla <karangc2@illinois.edu>
2017-02-13 23:57:50 -0600

    Open loop working

--- src/OpenLoopControl.cpp
+++ src/OpenLoopControl.cpp
@@ -1,15 +1,12 @@
-/*
- * rosserial Subscriber Example
- * Blinks an LED on callback
- */
 #include "mbed.h"
-#include <ros.h>
-#include <std_msgs/Empty.h>
-#include <std_msgs/Float32.h>
-#include <std_msgs/Int32.h>
-#include "Sabertooth.h"
 #include "QEI.h"
+#include <cstdlib>
+#include <ros.h>
+#include <std_msgs/Float32.h>
+#include "Sabertooth.h"
+#include <cmath>
 
+ 
 #ifdef TARGET_LPC1768
 #define MOTORSHIELD_IN1     p23
 #define MOTORSHIELD_IN2     p24
@@ -28,9 +25,15 @@
 #error "You need to specify a pin for the sensor"
 #endif
 
-QEI EncdrL(MOTORSHIELD_IN3, MOTORSHIELD_IN4, NC, 624);
-QEI EncdrR(MOTORSHIELD_IN1, MOTORSHIELD_IN2, NC, 624);
-Sabertooth sb(SPEEDPIN_A,129,9600);
+Ticker flipper;
+
+QEI wheelL(MOTORSHIELD_IN3, MOTORSHIELD_IN4, NC, 624);
+QEI wheelR(MOTORSHIELD_IN1, MOTORSHIELD_IN2, NC, 624);
+Sabertooth Sb(SPEEDPIN_A,129,9600);
+
+float L=0.28;
+float R=0.03;
+
 
 float lwheel_tangent_vel_target = 0;
 float rwheel_tangent_vel_target = 0;
@@ -44,13 +47,10 @@ float rwheel_angular_vel_enc = 0;
 
 int motor_cmd_max = 127;
 int motor_cmd_min = -127;
+float motor_max_angular_vel = 4233.334;
+float motor_min_angular_vel = -666.667;
 
-float R = 0.03;
-
-ros::NodeHandle nh;
-std_msgs::Float32 float_msg;
-std_msgs::Int32 int_msg;
-
+/*
 ros::Publisher lwheel_angular_vel_target_pub("lwheel_angular_vel_target", &float_msg);
 ros::Publisher rwheel_angular_vel_target_pub("rwheel_angular_vel_target", &float_msg);
 
@@ -59,47 +59,64 @@ ros::Publisher rwheel_angular_vel_control_pub("rwheel_angular_vel_control", &flo
 
 ros::Publisher lwheel_angular_vel_motor_pub("lwheel_angular_vel_motor", &int_msg);
 ros::Publisher rwheel_angular_vel_motor_pub("rwheel_angular_vel_motor", &int_msg);
+*/
 
-void lwheel_tangent_vel_callback( const std_msgs::Float32& msg)
+//Subscribing to left wheel and right wheel velocities
+void lwheel_tangent_vel_target_callback( const std_msgs::Float32& float_msg)
 {
-    lwheel_tangent_vel_target = msg.data;
+    lwheel_tangent_vel_target = (float)float_msg.data;
 }
 
-void rwheel_tangent_vel_callback( const std_msgs::Float32& msg)
+void rwheel_tangent_vel_target_callback( const std_msgs::Float32& float_msg)
 {
-    rwheel_tangent_vel_target = msg.data;
+    rwheel_tangent_vel_target = (float)float_msg.data;
 }
 
-void lwheel_angular_vel_callback( std_msgs::Int32& msg)
+//Read in encoder readings for PID
+void lwheel_angular_vel_enc_callback(const std_msgs::Float32& float_msg)
 {
-    lwheel_angular_vel_target = msg.data;
+	lwheel_angular_vel_enc = float_msg.data;
 }
 
-void rwheel_angular_vel_callback( std_msgs::Int32& msg)
+void rwheel_angular_vel_enc_callback(const std_msgs::Float32& float_msg)
 {
-    rwheel_angular_vel_target = msg.data;
+	rwheel_angular_vel_enc = float_msg.data;
 }
 
-//Read in tangential velocity targets
-ros::Subscriber<std_msgs::Float32> lwheel_tangent_vel_target_sub("lwheel_tangent_vel_target",lwheel_tangent_vel_callback);
-ros::Subscriber<std_msgs::Float32> rwheel_tangent_vel_target_sub("rwheel_tangent_vel_target",rwheel_tangent_vel_callback);
+//Subscriber initialization
+ros::Subscriber<std_msgs::Float32> lwheel_tangent_vel_target_sub("lwheel_tangent_vel_target", lwheel_tangent_vel_target_callback);
+ros::Subscriber<std_msgs::Float32> rwheel_tangent_vel_target_sub("rwheel_tangent_vel_target", rwheel_tangent_vel_target_callback);
+
+ros::Subscriber<std_msgs::Float32> lwheel_angular_vel_enc_sub("lwheel_angular_vel_enc", lwheel_angular_vel_enc_callback);
+ros::Subscriber<std_msgs::Float32> rwheel_angular_vel_enc_sub("rwheel_angular_vel_enc", rwheel_angular_vel_enc_callback);
 
 float tangentvel_2_angularvel(float tangent_vel)
 {
+	/*
+	V = omega * R
+	*/
     float angular_vel = tangent_vel/R;
     return angular_vel;
 }
 
+
+/*
+Mapping angular velocity targets to motor commands 
+Note: Moto commands are ints between -127 to 127
+*/
 int angularvel_2_motorcmd(float angular_vel_target)
 {
     int motor_cmd;
 
+    float slope = (motor_cmd_max - motor_cmd_min)/(motor_max_angular_vel - motor_min_angular_vel);
+    float intercept = 20;
     if (angular_vel_target ==0)
         return 0;
 
     if (angular_vel_target > 0)
     {
-        motor_cmd = 0; //Set this at the lab
+
+        motor_cmd = slope*(angular_vel_target) + intercept;
         if(motor_cmd > motor_cmd_max)
             motor_cmd = motor_cmd_max;
         if(motor_cmd < motor_cmd_min)
@@ -107,94 +124,69 @@ int angularvel_2_motorcmd(float angular_vel_target)
     }
     else 
     {
-        motor_cmd = 0; //Set this later
+        motor_cmd = slope*(angular_vel_target) + intercept;
         if(motor_cmd > motor_cmd_max)
             motor_cmd = motor_cmd_max;
         if(motor_cmd < motor_cmd_min)
             motor_cmd = motor_cmd_min;  
 
-        motor_cmd = -motor_cmd; 
     }
 
     return motor_cmd;
 }
 
+//Send motor commands to robot
 void motorcmd_2_robot( char wheel, int motor_command, Sabertooth &Sb)
 {
-    int motor_command_raw = (int) abs(motor_command);
-
-    if(wheel == 'l')
-    {
-        if(motor_command>=0)
-        {
-            Sb.SetSpeedMotorA(motor_command_raw);
-        }
-        else if(motor_command < 0)
-        {
-            Sb.SetSpeedMotorA(-motor_command_raw);
-        }
-    }
 
     if(wheel == 'r')
     {
-        if(motor_command>=0)
-        {
-            Sb.SetSpeedMotorB(motor_command_raw);
-        }
-        else if(motor_command < 0)
-        {
-            Sb.SetSpeedMotorB(-motor_command_raw);
-        }
+        Sb.SetSpeedMotorA(motor_command);
+    }
+
+    if(wheel == 'l')
+    {
+        Sb.SetSpeedMotorB(motor_command);
     }
 }
 
-void lwheel_update(Sabertooth &Sb)
+ros::NodeHandle nh;
+
+int main()
 {
-    lwheel_angular_vel_target = tangentvel_2_angularvel(lwheel_tangent_vel_target);
-    lwheel_angular_vel_target_pub.publish(lwheel_angular_vel_target);
-
-    lwheel_angular_vel_control_pub.publish(lwheel_angular_vel_target);
-
-    //Compute motor command
-    int lwheel_motor_cmd = angularvel_2_motorcmd(lwheel_angular_vel_target);
-    lwheel_angular_vel_motor_pub.publish(lwheel_motor_cmd); 
-
-    motorcmd_2_robot('l',lwheel_motor_cmd, Sb);
-
-}
-
-void rwheel_update(Sabertooth &Sb)
-{
-    rwheel_angular_vel_target = tangentvel_2_angularvel(rwheel_tangent_vel_target);
-    rwheel_angular_vel_target_pub.publish(rwheel_angular_vel_target);
-
-    rwheel_angular_vel_control_pub.publish(rwheel_angular_vel_target);
-
-    //Compute motor command
-    int rwheel_motor_cmd = angularvel_2_motorcmd(rwheel_angular_vel_target);
-    rwheel_angular_vel_motor_pub.publish(rwheel_motor_cmd); 
-
-    motorcmd_2_robot('r', rwheel_motor_cmd, Sb);
-
-}
-
-DigitalOut myled(LED1);
-
-void messageCb(const std_msgs::Empty& toggle_msg){
-    myled = !myled;   // blink the led
-}
-
-ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb);
-
-int main() {
     nh.initNode();
-    nh.subscribe(sub);
-    sb.InitializeCom();
+    nh.subscribe(lwheel_tangent_vel_target_sub);
+    nh.subscribe(rwheel_tangent_vel_target_sub);
+    nh.subscribe(lwheel_angular_vel_enc_sub);
+    nh.subscribe(rwheel_angular_vel_enc_sub);
 
-    while (1) {
-    	sb.SetSpeedMotorA((int)50);
-        sb.SetSpeedMotorB((int)-50);
+    Sb.InitializeCom();
+
+    while(1)
+    {
         nh.spinOnce();
         wait_ms(1);
+
+        rwheel_angular_vel_target = tangentvel_2_angularvel(rwheel_tangent_vel_target);
+        //rwheel_angular_vel_target_pub.publish(rwheel_angular_vel_target);
+
+        //rwheel_angular_vel_control_pub.publish(rwheel_angular_vel_target);
+        int rwheel_motor_cmd = angularvel_2_motorcmd(rwheel_angular_vel_target);
+        //rwheel_angular_vel_motor_pub.publish(rwheel_motor_cmd); 
+
+        motorcmd_2_robot('r', rwheel_motor_cmd, Sb);
+
+        lwheel_angular_vel_target = tangentvel_2_angularvel(lwheel_tangent_vel_target);
+        //lwheel_angular_vel_target_pub.publish(lwheel_angular_vel_target);
+
+        //lwheel_angular_vel_control_pub.publish(lwheel_angular_vel_target);
+
+        //Compute motor command
+        int lwheel_motor_cmd = angularvel_2_motorcmd(lwheel_angular_vel_target);
+        //lwheel_angular_vel_motor_pub.publish(lwheel_motor_cmd); 
+
+        motorcmd_2_robot('l',lwheel_motor_cmd, Sb);
+        wait_ms(500);
     }
+
 }
